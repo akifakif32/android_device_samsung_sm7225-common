60d221841ce3c57f4f560f9b398368d31ce5cac6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2026-05-24 19:40:49 -0300

    sm7225-common: Patch vendor.qti.hardware.camera.postproc@1.0-service-impl.so
    The modified library NOPs out the instruction to change binder
    threadpool, allowing the library to load and camera to work on S.
    
    Change-Id: I5e8a14243d06f9b0c72a22e6e3f0c3b3c5f9f3aa

--- extract-files.py
+++ extract-files.py
@@ -37,7 +37,7 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace(
             '60 0e 40 f9 82 0c 80 52 24 00 80 52 e1 03 15 aa 08 00 40 f9 e3 03 14 aa',
             '60 0e 40 f9 82 0c 80 52 24 00 80 52 e1 03 15 aa 08 00 40 f9 03 00 80 d2'
-        )
+        ),
     ('vendor/lib64/hw/gatekeeper.mdfpp.so', 'vendor/lib64/libkeymaster_helper.so', 'vendor/lib64/libskeymaster4device.so'): blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
     ('vendor/lib/libwvhidl.so', 'vendor/lib/mediadrm/libwvdrmengine.so'): blob_fixup()
@@ -48,6 +48,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbase_shim.so'),
     ('vendor/lib64/libdpps.so'): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    ('vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
+        .sig_replace('9A 0A 00 94', '1F 20 03 D5'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
