0eb51d273f71779d854b9e3ff4d8e4ac6aa3eda9
Aflaungos <vimavick@gmail.com>
2026-06-03 12:11:35 -0300

    sm7225-common: Fix extraction script namespaces
    Change-Id: I9a4c82a5f6443c9f1156a655378e12b7709c3baa

--- extract-files.py
+++ extract-files.py
@@ -23,6 +23,8 @@ namespace_imports = [
     'hardware/qcom-caf/sm8250',
     'hardware/qcom-caf/wlan',
     'hardware/samsung',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
 ]
