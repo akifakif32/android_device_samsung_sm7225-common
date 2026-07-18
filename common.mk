42aa22760436a0a2392299c68c690494c7d29ffa
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-18 13:57:26 +0300

    sm7225-common: remove unnecessary modules

--- common.mk
+++ common.mk
@@ -72,10 +72,6 @@ PRODUCT_PACKAGES += \
     init.qti.early_init.sh \
     init.qti.qcv.sh
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default \
-    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
-
 $(call soong_config_set,libinit,vendor_init_lib,//$(COMMON_PATH):libinit_sm7225)
 
 # Audio
